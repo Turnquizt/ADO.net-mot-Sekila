bf940140f086a8964bdca9cd5a45a20d83d59724
Turnquizt <tobbe_n_95@hotmail.com>
2024-12-10 17:49:50 +0100

    Add files via upload

---
+++ ADO.net koppling mot Sakila/ADO.net koppling mot Sakila/SakilaDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.net_koppling_mot_Sakila
+{
+    internal class SakilaDbContext
+    {
+        public void FetchFilmsByActorName(string aFirstName, string aLastName)
+        {
+            var connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Sakila;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            var command1 = new SqlCommand("Select actor.actor_id, film.film_id, film.title " +
+                                          "From dbo.film_actor " +
+                                          "Inner join dbo.film on film_actor.film_id = film.film_id " +
+                                          "Inner Join dbo.actor on film_actor.actor_id = actor.actor_id " +
+                                          $"Where actor.first_name = '{aFirstName}' and actor.last_name = '{aLastName}'", connection);
+            connection.Open();
+            using (var rec = command1.ExecuteReader())
+            {
+
+                if (rec.HasRows)
+                {
+                    Console.WriteLine($"{aFirstName} {aLastName} has acted in movies:");
+
+                    while (rec.Read())
+                    {
+
+                        Console.WriteLine(rec["title"]);
+                    }
+                }
+
+
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ ADO.net koppling mot Sakila/ADO.net koppling mot Sakila/UserMenu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.net_koppling_mot_Sakila
+{
+    internal class UserMenu
+    {
+        SakilaDbContext SDbCtxt = new SakilaDbContext();
+        public void InputActorAndFetchFilms() 
+        {
+            Console.WriteLine("Please enter the firstname of the actor you want to look up.");
+            var actorFirstName = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Please enter the lastname of the actor you want to look up.");
+            var actorLastName = Console.ReadLine();
+            Console.Clear();
+            SDbCtxt.FetchFilmsByActorName(actorFirstName, actorLastName);
+        }
+    }
+}
